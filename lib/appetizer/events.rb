93dc23b283ec2dff93c94a5ead6a05fffa7829c5
John Barnette <john@jbarnette.com>
2011-09-06 20:39:17 -0500

    Infinite loops are stupid.

--- lib/appetizer/events.rb
+++ lib/appetizer/events.rb
@@ -5,11 +5,11 @@ module Appetizer
     end
 
     def hooks
-      @hooks ||= Hash.new { |h, k| h[k] = h[k.to_s] || [] }
+      @hooks ||= Hash.new { |h, k| Symbol === k ? h[k.to_s] : [] }
     end
 
     def on event, &block
-      hooks[event] << block
+      hooks[event.to_s] << block
     end
   end
 end
