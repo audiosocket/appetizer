7850748ba853207dade025f8e015f443ac853825
John Barnette <john@jbarnette.com>
2011-09-09 23:06:38 -0500

    Allow additive setup/teardown in tests.

--- lib/appetizer/test.rb
+++ lib/appetizer/test.rb
@@ -1,9 +1,39 @@
 ENV["RACK_ENV"] = ENV["RAILS_ENV"] = "test"
 
-require "appetizer/init"
 require "minitest/autorun"
 
 module Appetizer
   class Test < MiniTest::Unit::TestCase
+    def setup
+      self.class.setups.each { |s| instance_eval(&s) }
+    end
+
+    def teardown
+      self.class.teardowns.each { |t| instance_eval(&t) }
+    end
+
+    def self.setup &block
+      setups << block
+    end
+
+    def self.setups
+      @setups ||= (ancestors - [self]).
+        map { |a| a.respond_to?(:setups) && a.setups }.
+        select { |s| s }.
+        compact.flatten.reverse
+    end
+
+    def self.teardown &block
+      teardowns.unshift block
+    end
+
+    def self.teardowns
+      @teardowns ||= (ancestors - [self]).
+        map { |a| a.respond_to?(:teardowns) && a.teardowns }.
+        select { |t| t}.
+        compact.flatten
+    end
   end
 end
+
+require "appetizer/init"
