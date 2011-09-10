a6258d7040ade95ff76fd8e80f3d48686709167e
John Barnette <john@jbarnette.com>
2011-09-09 23:06:21 -0500

    Fix connection.

--- lib/appetizer/activerecord.rb
+++ lib/appetizer/activerecord.rb
@@ -9,13 +9,14 @@ def App.arconfig!
   url = URI.parse ENV["DATABASE_URL"]
 
   cfg = {
-    adapter:  url.scheme,
-    database: url.path[1..-1],
-    encoding: "utf8",
-    host:     url.host,
-    password: url.password,
-    port:     url.port,
-    username: url.user
+    adapter:      url.scheme,
+    database:     url.path[1..-1],
+    encoding:     "utf8",
+    host:         url.host,
+    min_messages: "WARNING",
+    password:     url.password,
+    port:         url.port,
+    username:     url.user
   }
 
   case url.scheme
@@ -33,4 +34,46 @@ end
 App.on :initializing do
   App.arconfig!
   ActiveRecord::Base.establish_connection App.env
+end
+
+if defined? Appetizer::Test  
+  class Appetizer::Test
+    module Fixtures
+      def self.included klass
+        fixtures = Dir["test/fixtures/*.yml"].map { |f| File.basename f, ".yml" }
+
+        unless fixtures.empty?
+          require "active_record/fixtures"
+          ActiveRecord::Fixtures.create_fixtures "test/fixtures", fixtures, {}
+
+          fixtures.each do |fixture|
+            model = fixture.classify.constantize
+
+            define_method fixture do |name|
+              model.find ActiveRecord::Fixtures.identify name
+            end
+          end
+        end
+      end
+    end
+
+    module Transactional
+      def run runner
+        result = nil
+
+        ActiveRecord::Base.transaction do
+          result = super
+          raise ActiveRecord::Rollback
+        end
+
+        result
+      end
+    end
+
+    include Transactional
+
+    setup do
+      ActiveRecord::IdentityMap.clear
+    end
+  end
 end
